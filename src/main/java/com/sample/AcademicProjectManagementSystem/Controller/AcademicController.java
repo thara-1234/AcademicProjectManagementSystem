cede034b881e0698b28b3c06ed8e87d2df8a6f10
thara-1234 <tharakrishna1989@gmail.com>
2023-06-09 08:27:15 +0530

    admin providing username and password solved

--- src/main/java/com/sample/AcademicProjectManagementSystem/Controller/AcademicController.java
+++ src/main/java/com/sample/AcademicProjectManagementSystem/Controller/AcademicController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 
 
 @Controller
@@ -18,8 +19,18 @@ public class AcademicController {
     public void createUsers(@RequestBody Users users) throws Exception {
         academicService.createAUser(users);
     }
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+    public void updateUser(@PathVariable int id, @RequestBody Map<String, String> userUpdateMap) throws Exception {
+        Users users = academicService.getUserById(id);
 
-
+        if (userUpdateMap.containsKey("username")) {
+            users.setUsername(userUpdateMap.get("username"));
+        }
+        if (userUpdateMap.containsKey("password")) {
+            users.setPassword(userUpdateMap.get("password"));
+        }
+        academicService.updateUser(users);
+    }
 }
 
 
--- src/main/java/com/sample/AcademicProjectManagementSystem/Service/AcademicService.java
+++ src/main/java/com/sample/AcademicProjectManagementSystem/Service/AcademicService.java
@@ -6,6 +6,15 @@ import com.sample.AcademicProjectManagementSystem.Entities.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -15,33 +24,32 @@ import static com.sample.AcademicProjectManagementSystem.Enum.UserRole.*;
 public class AcademicService {
     @Autowired
     UsersRepository usersRepository;
-    public void createAUser(Users users) throws Exception {
 
-        if (users.getRole().equals(ADMIN)) {
-            users.setRole(ADMIN);
-        } else if (users.getRole().equals(UserRole.HOD)) {
-            users.setRole(UserRole.HOD);
-        } else if (users.getRole().equals(UserRole.INTERNAL_GUIDE)) {
-            users.setRole(UserRole.INTERNAL_GUIDE);
-        } else if (users.getRole().equals(UserRole.PROJECT_IN_CHARGE)) {
-            users.setRole(UserRole.PROJECT_IN_CHARGE);
-        } else {
-            users.setRole(UserRole.STUDENT);
-        }
+    final String SECRET_KEY = "a1b2c3d4e5f6g7h8i9j10k11l12m13n1";
 
-        if (users.getPhNo().length() != 10) {
+    public void createAUser(Users users) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+        if (users.getPhNo().length()!=10) {
             throw new IllegalArgumentException("Invalid Phone Number");
-        }
-        if(usersRepository.countByEmailId(users.getEmailId())>0){
-            throw new Exception("Already registered emailId");
         }
 
         if (!users.getEmailId().endsWith(".com")) {
             throw new IllegalArgumentException("Invalid Mail ID");
         }
+        if (users.getPassword().length()<=6 && users.getPassword().matches("[a-zA-Z0-9]")) {
+            throw new IllegalArgumentException("Invalid Password");
+        }
         usersRepository.save(users);
-    }}
+    }
+    public Users getUserById(int id) {
+        return usersRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("User not found with ID: " + id));
+    }
 
+    public void updateUser(Users users) {
+        usersRepository.save(users);
+    }
+}
 
 
 
